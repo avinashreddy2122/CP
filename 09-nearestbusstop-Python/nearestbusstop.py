146d09997bfe6f25cb923554b0d954dc55db222a
avinashreddy <avimakireddy@msitprogram.net>
2021-07-14 19:12:06 +0530

    done

--- 09-nearestbusstop-Python/nearestbusstop.py
+++ 09-nearestbusstop-Python/nearestbusstop.py
@@ -4,8 +4,36 @@
 # including street 0, and ties go to the lower street, 
 # so the nearest bus stop to 12th street is 8th street, 
 # and the nearest bus stop to 13 street is 16th street.
-
-
-
 def fun_nearestbusstop(street):
-	return -1
+	a=round(street/8)
+	print(a)
+	if street <=4:
+		return 0
+	if street%8==0:
+		return street
+	b=str(street/8).split(".")
+	print(b)
+	c=b[1][::-1]
+	d=int(c)%10
+
+	
+	print(d)
+	if d >5 :
+		return a*8
+	else:
+		return (a-1)*8
+	
+print(fun_nearestbusstop(164))
+
+
+# 	print(a)
+# 	b=a*8
+# 	print(b)
+# 	c=b-street
+# 	print(c)
+# 	if c < 5:
+# 		print((a*8)-street)
+# 		print((a-1)*8)
+# 		return (a-1)*8
+# 	else:
+# 		return a*8
