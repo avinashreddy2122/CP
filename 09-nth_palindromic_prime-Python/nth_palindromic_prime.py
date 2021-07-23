e75167df252aa313e0d71e2b26d50129d2f153c4
avinashreddy <avimakireddy@msitprogram.net>
2021-07-23 22:34:06 +0530

    done

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -7,5 +7,28 @@
 # so 313 is an palindrome Prime. fun_nth_palindrome_prime(0) 
 # returns 2
 
+def isPrime(n):
+	if n<2:
+		return False
+	for factor in range(2,n):
+		if n%factor ==0:
+			return False
+	return True
+def isPalindrome(n):
+	rev =0
+	while (n>0):
+		dig = n%10
+		rev = rev*10+dig
+		n = n//10
+	return rev
+
 def fun_nth_palindromic_prime(n):
-	return 0
+	num=2
+	count=0
+	while(count<=n):
+
+		if (isPrime(num) and isPalindrome(num)==num):
+			count+=1
+		num+=1
+	return num-1
+print(fun_nth_palindromic_prime(15))
