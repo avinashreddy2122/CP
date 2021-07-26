28f706e1c5f306a5313c21ed016e979337708d0d
avinashreddy <avimakireddy@msitprogram.net>
2021-07-26 23:39:35 +0530

    done

--- 09-median-Python/median.py
+++ 09-median-Python/median.py
@@ -5,4 +5,17 @@
 
 def median(a):
 	# your code goes here
-	pass
+	n = len(a)
+	if n==0:
+		return None
+	a.sort()
+	if n%2 == 0:
+		median1 = a[n//2]
+		# print(median1)
+		median2 = a[n//2 -1]
+		# print(median2)
+		median = (median1+median2)/2
+	else:
+		median = a[n//2]
+	return (median)
+print(median([1.1, 2.1, 3.1, 4.1, 5.1]))
