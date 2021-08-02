de2bce50b52385a2dd9b9c03a9a023fd294d3032
avinashreddy <avimakireddy@msitprogram.net>
2021-08-02 18:02:38 +0530

    done

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -5,6 +5,21 @@
 # fun_nth_tidynumber(5) = 6
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
-
+def tidyNum(n):
+    prev = 10
+    while(n>0):
+        num=n%10
+        n=n//10
+        if num>prev:
+            return False
+        prev=num
+    return True
 def fun_nth_tidynumber(n):
-    return 0
+    num=0
+    cnt=0
+    while(cnt<=n):
+        num+=1
+        if (tidyNum(num)):
+            cnt=cnt+1
+        
+    return num
